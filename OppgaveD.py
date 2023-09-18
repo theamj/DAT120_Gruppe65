23be3d018f0140062fe660e6b8c4d32d35d28d6b
theamj <117945968+theamj@users.noreply.github.com>
2023-09-18 15:15:48 +0200

    Add files via upload

---
+++ OppgaveD.py
@@ -0,0 +1,3 @@
+#Skriv en funksjon som tar inn ei liste med flyttall og en enkeltverdi. 
+#Funksjonen skal telle antall elementer i lista som er større enn eller 
+#lik den oppgitte verdien og returnere dette.
