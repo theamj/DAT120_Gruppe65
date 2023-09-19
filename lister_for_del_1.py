7002c4a02ab52eba9b7f7bfc8e34c4eb29395afe
Waffles <marcus.t.eikrem@gmail.com>
2023-09-19 16:47:33 +0200

    Legger til filen med listene for del 1

---
+++ lister_for_del_1.py
@@ -0,0 +1,6 @@
+temperaturer = [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18, 21, 26, 21, 31, 15, 4, 1, -2]
+dogn_nedbor = [2, 5, 0, 0, 0, 3, 6, 4, 0, 0, 5, 0, 12, 12, 12, 12, 7, 19]
+
+temperaturer_tidspunkter = list()
+for index in range(len(temperaturer)):
+    temperaturer_tidspunkter.append(index)
