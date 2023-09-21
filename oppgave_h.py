479fdc552c9a3f32706e457dafa0d2d43a1fc58c
DridasBandidas <idaueland89@gmail.com>
2023-09-21 14:32:03 +0200

    Ryddet
    Laget mer oversikt i oppgave H

--- oppgave_h.py
+++ oppgave_h.py
@@ -4,21 +4,63 @@ Created on Wed Sep 20 11:23:04 2023
 
 @author: 47954
 """
+
 testliste = [4,7,15,]
+testliste2 = [15,7,4]
 
-def g_rate(n):
-    if n < 5: 
-       return 0
-    else:
-        return n - 5
+# Oppgave H)    En plante vil vokse ved en temperatur over 5 grader. 
+#               Ettersom tempereturen øker, vokser planten linjært fortere.
+#               Lager en funksjon som regner ut den totale veksten.
 
-
-def growth(list):
+def pos_growth(list):
     g_units = 0
     for i in range(len(list)): 
-        g_units = g_units + g_rate((list)[i])
+     g_rate = (list)[i] - 5
+     g_units = g_units + g_rate
     return(g_units)
 
 
 if __name__==  "__main__":
-    print(f"Planten har vokst totalt: {growth(testliste)} enheter.")
+    print(pos_growth(testliste))
+
+
+#  Frivillig A) Tar med negativ vekst, planten dør hvis vekst når 0.
+     
+def growth(list):
+    g_units = 0
+    for i in range(len(list)): 
+        g_rate = (list)[i] - 5
+        g_units = g_units + g_rate
+        if g_units <= 0:
+            return 0
+    return g_units
+
+if __name__==  "__main__":
+   
+    print(growth(testliste))
+
+
+# Frivillig B)  Returner i tilegg periode og størrelse av størst vekst.
+#               !!!Har misforstått oppgaven!!
+def results(list):     
+    
+    g_units = 0
+    g_max = 0
+    d_max = 0
+    
+    for i in range(len(list)): 
+       
+        g_rate = (list)[i] - 5
+        g_units = g_units + g_rate
+               
+        if g_units <= 0:
+            return [i, d_max, g_max]
+       
+        if g_rate > g_max:
+            g_max = g_rate
+            d_max = i
+    return [g_units, i, g_max]
+        
+
+if __name__==  "__main__":
+    print(results(testliste2))
--- oppgave_l.py
+++ oppgave_l.py
@@ -8,5 +8,5 @@ Created on Wed Sep 20 13:06:35 2023
 import lister_for_del_1
 import oppgave_h
 
-G = oppgave_h.growth(lister_for_del_1.temperaturer)
+G = oppgave_h.pos_growth(lister_for_del_1.temperaturer)
 print(f"Planten har vokst med totalt {G} enheter.")
