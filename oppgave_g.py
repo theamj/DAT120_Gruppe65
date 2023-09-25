805740713702b3d765e8f6f36df3808a5653160f
WeesKneak <145125951+WeesKneak@users.noreply.github.com>
2023-09-25 15:24:14 +0200

    Create oppgave_g.py

---
+++ oppgave_g.py
@@ -0,0 +1,18 @@
+"""
+Skriv en funksjon som regner ut hva trenden i et datasett er. Datasettet skal bestå av to
+lister, ei liste med x-koordinater og ei liste med y-koordinater. Elementene på samme indeks
+i de to listene hører sammen. Trenden skal beregnes som to tall a og b, som skal fungere som
+parametere i en lineær formel: verdi = ax + b. Implementer følgende formler, hvor n er til
+men ikke med lengden til lista, xi er i-ende element i lista x, og 𝑥 er gjennomsnittet av alle
+verdiene i lista x. Merk at disse formlene og hvorfor de ser slik ut er pensum i emnet STA100,
+temaet lineær regresjon, minste kvadraters metode.
+𝑎 = ∑ (𝑥𝑖 − 𝑥)(𝑦𝑖 − 𝑦)𝑛
+𝑖=0
+∑ (𝑥𝑖 − 𝑥)2𝑛
+𝑖=0
+𝑏 = 𝑦 − 𝑎 ∗ 𝑥
+"""
+
+
+
+
